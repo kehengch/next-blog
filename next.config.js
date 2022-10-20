1bc045aab7b633fdaeb94e367665ab77f4b895f2
can-dy-jack <jjack_chen3@163.com>
2022-10-20 17:44:29 +0800

    fix01

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  basePath: '/next-blog'
 }
 
 module.exports = nextConfig
